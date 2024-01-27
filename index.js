763a1a7b08a0728d214d5119580d907c3982cf5e
SCHT001 <sachetsubedi001@gmail.com>
2024-01-27 21:36:55 +0545

    initial

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Resonse");
+});
+const httpServer=app.listen(3000, console.log("Server running at 3000"));
+const io= new Server(httpServer,{
+    cors:{
+        origin:'*'
+    }
+})
+
+io.on('connection',(socket)=>{
+    
+    const defaultNames = ['Anonymous Monkey', 'Captain Quirk', 'Luna Stardust', 'Sunny Sideup', 'Pixel Pajamas', 'Whispering Willow', 'Captain Noodle', 'Mystic Muffin', 'Jazzberry Jam', 'Dizzy Dinosaur', 'Cheesequake Cactus', 'Moonlight Marmalade', 'Sir Snugglekins', 'Chuckleberry Finn', 'Bubbles McFluff', 'Doodlebug Daffodil', 'Spaghetti Supernova', 'Giggles McGee', 'Scribble Scribble', 'Twinkle Toes'];
+    let userName;
+      
+    // send connection message
+    socket.on('createConnection',(name)=>{
+        name!=null?userName=name:userName=defaultNames[Math.floor(Math.random()*20)];
+        socket.broadcast.emit('connected',userName);
+        socket.emit('connectionSuccess',userName);
+    })
+    // message sent
+    socket.on('message',(mess)=>{
+        console.log(mess);
+        io.emit('message',{userName:userName,message:mess});
+    })
+
+    
+    // rename user
+    socket.on('rename',(newUserName)=>{
+        const oldUsername=userName;
+        userName=newUserName;
+        io.emit('rename',{oldUsername,userName});
+    })
+
+    socket.on('disconnect',(reason)=>{
+        socket.broadcast.emit('disconnected',userName);
+    })
+    
+})
