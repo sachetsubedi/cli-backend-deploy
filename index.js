9f6574e0d100c03a64817f82780359d88c8bba1d
SCHT001 <sachetsubedi001@gmail.com>
2024-01-28 14:10:22 +0545

    join creation, join

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { Server } from "socket.io";
+import generateRandomRoomName from './scripts/randomRoomGenerator.js';
 import cors from "cors";
 const app = express();
 app.use(cors());
@@ -18,17 +19,37 @@ io.on('connection',(socket)=>{
     
     const defaultNames = ['Anonymous Monkey', 'Captain Quirk', 'Luna Stardust', 'Sunny Sideup', 'Pixel Pajamas', 'Whispering Willow', 'Captain Noodle', 'Mystic Muffin', 'Jazzberry Jam', 'Dizzy Dinosaur', 'Cheesequake Cactus', 'Moonlight Marmalade', 'Sir Snugglekins', 'Chuckleberry Finn', 'Bubbles McFluff', 'Doodlebug Daffodil', 'Spaghetti Supernova', 'Giggles McGee', 'Scribble Scribble', 'Twinkle Toes'];
     let userName;
+    let roomId='master';
+    socket.join(roomId);
+
+    socket.on('joinRoom',(currentRoom,room)=>{
+        socket.leave(currentRoom);
+        socket.join(room);
+        socket.emit('roomJoined',room);
+        socket.broadcast.to(room).emit('join',userName);
+        roomId=room;
+    })
+
+    socket.on('createRoom',(currentRoom)=>{
+        const room=generateRandomRoomName();
+        socket.leave(currentRoom);
+        socket.join(room);
+        socket.emit('roomCreated',room);
+        socket.emit('roomJoined',room);
+        roomId=room;
+    })
+
       
     // send connection message
     socket.on('createConnection',(name)=>{
         name!=null?userName=name:userName=defaultNames[Math.floor(Math.random()*20)];
-        socket.broadcast.emit('connected',userName);
+        socket.broadcast.to(roomId).emit('connected',userName);
         socket.emit('connectionSuccess',userName);
     })
     // message sent
     socket.on('message',(mess)=>{
         console.log(mess);
-        io.emit('message',{userName:userName,message:mess});
+        io.to(roomId).emit('message',{userName:userName,message:mess});
     })
 
     
@@ -36,7 +57,7 @@ io.on('connection',(socket)=>{
     socket.on('rename',(newUserName)=>{
         const oldUsername=userName;
         userName=newUserName;
-        socket.broadcast.emit('rename',{oldUsername,userName});
+        socket.broadcast.to(roomId).emit('rename',{oldUsername,userName});
         socket.emit('renamed',{oldUsername,userName});
     })
 
---
+++ scripts/randomRoomGenerator.js
@@ -0,0 +1,12 @@
+function generateRandomRoomName() {
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    let randomRoomName = '';
+  
+    for (let i = 0; i < 5; i++) {
+      const randomIndex = Math.floor(Math.random() * letters.length);
+      randomRoomName += letters[randomIndex];
+    }
+  
+    return randomRoomName;
+  }
+  export default generateRandomRoomName;
